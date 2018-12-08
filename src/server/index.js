3d70a954f30dc998ff45369da6593be32e7457aa
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2018-12-08 11:44:18 +0100

    kk

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.port || 5000;
-const buildPath = path.join(__dirname, '../../Build');
+const buildPath = path.join(__dirname, "../../Build");
 
 app.use(express.static(buildPath));
 
+app.get("/*", function(req, res) {
+  const indexPath = path.join(buildPath, "index.html");
+  res.sendFile(indexPath);
+});
 
 app.listen(port, () => {
-    console.log("Express app is talking from port ${port}");
-} );
+  console.log("Express app is talking from port ${port}");
+});
