3f09849ab6f4012453d6007d215c70b48552e1fa
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2018-12-08 13:21:59 +0100

    new changes

--- src/client/App.js
+++ src/client/App.js
@@ -7,9 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hello World!</p>
           Learn React
         </header>
       </div>
