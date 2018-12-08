c56f6085c3108348a61511c929cc203a6a4bec6e
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2018-12-08 15:56:33 +0100

    atlast metors.json file works!!!

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 9001;
 const buildPath = path.join(__dirname, "../../build");
-
+const mentorsFilePath = path.join(__dirname, "./api/Mentors.json");
+app.use(bodyParser.json());
 app.use(express.static(buildPath));
+
+app.get("/api/mentors", (req, res) => {
+  fs.readFile(mentorsFilePath, "utf8", (err, data) => {
+    try {
+      res.send(data);
+    } catch (error) {
+      throw new Error(err);
+    }
+  });
+});
 
 app.get("/*", function(req, res) {
   const indexPath = path.join(buildPath, "index.html");
