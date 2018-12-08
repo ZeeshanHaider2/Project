334eaae59d3d1c77918eac8fc86bce496c360b92
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2018-12-08 13:34:23 +0100

    new changes

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
-          <p>Hello World!</p>
-          Learn React
-=======
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-            Hello worlds!
+            Edit <code>src/App.js</code> and save to reload. Hello worlds!
           </p>
-            Learn React
->>>>>>> 0d0d4b4423ea67cbe3f0f49e06122022e10ed8c1
+          Learn React
         </header>
       </div>
     );
