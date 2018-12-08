1290592194cb024c8b8114b54a26dcfe475b3073
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2018-12-08 13:36:02 +0100

    chnaged process.env.port to process.env.PORT in server/App.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.port || 9001;
+const port = process.env.PORT || 9001;
 const buildPath = path.join(__dirname, "../../build");
 
 app.use(express.static(buildPath));
